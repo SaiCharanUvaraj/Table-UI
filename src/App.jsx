5c248df17f33b3deaccb3671cc88226ef0956c24
SaiCharanUvaraj <saicharanuvi01@gmail.com>
2025-02-23 00:01:50 +0530

    implemented sorting

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import data from './db.json';
 import Form from './Form';
 import Navbar from './Navbar';
-import { FaEllipsisV, FaArrowLeft, FaArrowRight, FaRegSquare, FaSquareFull } from "react-icons/fa";
+import {FaArrowLeft, FaArrowRight, FaRegSquare, FaSort, FaSortUp, FaSortDown, FaCheckSquare, FaTrash } from "react-icons/fa";
 
 export const App = () => {
   function capitalize(str) {
@@ -11,96 +11,118 @@ export const App = () => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
-  const [info,setInfo]=useState(data);
-  const [add, setAdd] = useState(false);
+  /*useStates .... */
+  const [info, setInfo] = useState(data);
   const [select, setSelect] = useState(false);
-  const [multiSelect,setMultiSelect]=useState(false);
+  const [multiSelect, setMultiSelect] = useState(false);
   const [selectedItems, setSelectedItems] = useState([]);
+  const [searchValues, setSearchValues] = useState({ name: "", user: "", owner: "", domain: "", status: "", version: "", os: "" });
+  const [page, setPage] = useState({ start: 0, end: 1 });
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
 
+  /* sorting functionalities.... */
+  const handleSort = (key) => {
+    setSortConfig((prev) => ({ key, direction: prev.key === key && prev.direction === 'asc' ? 'desc' : 'asc', }));
+  };
 
-  const [searchValues, setSearchValues] = useState({name: "", user: "", owner: "", domain: "", status: "", version: "", os: ""});
-  const [page, setPage] = useState({start:0, end:1});
+  let sortedData = [...info];
 
+  if (sortConfig.key) 
+    {
+      sortedData.sort((a, b) => {
+        const valueA = a[sortConfig.key].toLowerCase();
+        const valueB = b[sortConfig.key].toLowerCase();
+        if (valueA < valueB) return sortConfig.direction === 'asc' ? -1 : 1;
+        if (valueA > valueB) return sortConfig.direction === 'asc' ? 1 : -1;
+        return 0;
+    });
+  }
+
+  /* Paging functionalities... */
   const pageRanges = [];
-  for (let i = 0; i <= info.length; i += 6)
+  for (let i = 0; i <= info.length; i += 10)
     pageRanges.push(i);
   if (pageRanges[pageRanges.length - 1] !== info.length)
     pageRanges.push(info.length);
 
-  const handleSearchChange = (e, key) => {
-    setSearchValues({ ...searchValues, [key]: e.target.value });
+  const handleNextPageChange = () => {
+    if (page.end === pageRanges.length - 1) return;
+    setPage({ start: page.start + 1, end: page.end + 1 });
   };
 
-  const handleNextPageChange = ()=>{
-    if(page.end===pageRanges.length-1)
-      return
-    const nextPage={start:page.start+1,end:page.end+1}
-    setPage(nextPage)
-  }
+  const handlePrevPageChange = () => {
+    if (page.start === 0) return;
+    setPage({ start: page.start - 1, end: page.end - 1 });
+  };
 
-  const handlePrevPageChange = ()=>{
-    if(page.start===0)
-      return
-    const nextPage={start:page.start-1,end:page.end-1}
-    setPage(nextPage)
-  }
+  const pagedData = sortedData.slice(pageRanges[page.start], pageRanges[page.end]);
 
-  const pagedData = info.slice(pageRanges[page.start],pageRanges[page.end])
+  /* Searching functionalities... */
+  const handleSearchChange = (e, key) => {
+    setSearchValues({ ...searchValues, [key]: e.target.value });
+  }; 
 
   const filteredData = pagedData.filter((item) =>
     Object.keys(searchValues).every((key) =>
       item[key].toLowerCase().includes(searchValues[key].toLowerCase())
-  ));
+    )
+  );
 
-  const handleChoose = (item)=>{
-    const newItems=[...selectedItems,item];
-    setSelectedItems(newItems);
-  }
-  const handleRemoveChoose = (item)=>{
-    const newItems=selectedItems.filter(i=>i!=item);
-    setSelectedItems(newItems);
-  }
+  /*indexing final data ... */
+  const finalData = filteredData.map((item, index) => ({...item,
+    index
+  }));
+  const indexedData=info.map((item, index) => ({...item,
+    index
+  }));
 
-  const handleDelete = ()=>{
-    if(selectedItems.length===0)
-      return
-    alert(`${selectedItems.length}will be deleted !`);
-    const newItems=info.filter((item)=>!selectedItems.includes(item.index))
-    setInfo(newItems);
+  /* Multi selecting functionalities ... */
+  const handleChoose = (item) => {
+    setSelectedItems([...selectedItems, item]);
+  };
+
+  const handleRemoveChoose = (item) => {
+    setSelectedItems(selectedItems.filter(i => i !== item));
+  };
+
+  const handleDelete = () => {
+    if (selectedItems.length === 0) return;
+    alert(`${selectedItems.length} will be deleted!`);
+    setInfo(indexedData.filter(item => !selectedItems.includes(item.index)));
     setSelectedItems([]);
-  }
+    setMultiSelect(false);
+  };
 
   return (
     <div>
-      <Navbar setForm={setAdd} select={select} setSelect={setSelect} info={info} setMultiSelect={setMultiSelect} handleDelete={handleDelete} multiSelect={multiSelect}/>
+      <Navbar select={select} setSelect={setSelect} info={info} setMultiSelect={setMultiSelect} handleDelete={handleDelete} multiSelect={multiSelect} />
       <div className='table'>
         <div className='row' style={{ backgroundColor: "lightblue" }}>
           {["name", "user", "owner", "domain", "status", "version", "os"].map((key) => (
-            <div key={key} className='column'>
-              <p className='header'>{capitalize(key)}</p>
+            <div key={key} className='column' style={{ cursor: "pointer" }}>
+              <div style={{ display: "flex", gap: "0.5rem", justifyContent: "flex-start", alignItems: "center" }} className="sortable-header" onClick={() => handleSort(key)}>
+                <p className='header'>{capitalize(key)}</p>
+                {sortConfig.key === key ? (sortConfig.direction === 'asc' ? <button style={{ cursor: "pointer" }}>sort-desc</button> : <button style={{ cursor: "pointer" }}>sort-asc</button>):""}
+                {sortConfig.key === key ? ( sortConfig.direction === 'asc' ? <FaSortUp /> : <FaSortDown /> ) : ( <FaSort className='sort-icon' /> )}
+              </div>
               {select &&
-                <input
-                  type="text"
-                  placeholder={`Search ${capitalize(key)}`}
-                  value={searchValues[key]}
-                  onChange={(e) => handleSearchChange(e, key)}
-                  className="search-input"
-                />
+                <input type="text" placeholder={`Search ${capitalize(key)}`} value={searchValues[key]} onChange={(e) => handleSearchChange(e, key)} style={{width:"80%",height:"1.5rem"}}/>
               }
             </div>
           ))}
           <div className='column'>
-            <p className='header'>Action</p>
+            <p className='header' style={{textAlign:"center"}}>Action</p>
           </div>
         </div>
-        {filteredData.map((item) => (
-          <div key={item.index} className='row' style={{ backgroundColor: item.index % 2 === 0 ? "ghostwhite" : "lightgray" }}>
-            <p className='text' style={{display:"flex",alignItems:"center",gap:"1rem"}}>
+        {finalData.map((item) => (
+          <div key={item.index} className='row' style={{ backgroundColor: item.index % 2 === 0 ? "ghostwhite" : "lightgray" }} 
+          onClick={()=>setSortConfig({ key: null, direction: 'asc' })}>
+            <p className='text' style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
               {multiSelect && !selectedItems.includes(item.index) &&
-                <p><FaRegSquare color="blue" onClick={()=>handleChoose(item.index)} /></p>
+                <FaRegSquare color="blue" onClick={() => handleChoose(item.index)} />
               }
               {multiSelect && selectedItems.includes(item.index) &&
-                <p><FaSquareFull color="blue" onClick={()=>handleRemoveChoose(item.index)} /></p>
+                <FaCheckSquare color="blue" onClick={() => handleRemoveChoose(item.index)} />
               }
               {capitalize(item.name)}
             </p>
@@ -110,22 +132,17 @@ export const App = () => {
             <p className='text'>{capitalize(item.status)}</p>
             <p className='text'>{capitalize(item.version)}</p>
             <p className='text'>{capitalize(item.os)}</p>
-            <p className='text'><FaEllipsisV /></p>
+            <p className='text' style={{textAlign:"center"}}><FaTrash /></p>
           </div>
         ))}
         <div className='paging'>
-            {multiSelect &&
-              <p>{selectedItems.length} items selected</p>
-            }
-          <div style={{display:"flex",gap:"1rem"}}>
+          <p className='bold-large' style={{ color: "darkgray" }}>Rows per Page: 6</p>
+          <p className='bold-large'>{pageRanges[page.start] + 1} - {pageRanges[page.end]} of {info.length}</p>
+          <div className='bold-large' style={{ display: "flex", gap: "1rem" }}>
             <FaArrowLeft onClick={handlePrevPageChange} />
             <FaArrowRight onClick={handleNextPageChange} />
           </div>
-          <p>{pageRanges[page.start]+1} to {pageRanges[page.end]}</p>
         </div>
-      </div>
-      <div style={{display:"grid",justifyContent:"center",alignContent:"center"}}>
-      {add && <Form setForm={setAdd} info={info} setInfo={setInfo}/>}
       </div>
     </div>
   );
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,22 +1,60 @@
-import React from 'react'
-import { FaRegSquare, FaTrash } from 'react-icons/fa';
-import { FaSearch,FaSort,FaList} from 'react-icons/fa';
+import React, { useState, useEffect, useRef } from "react";
+import { FaFileExport, FaRegSquare, FaTrash, FaSearch, FaPlus } from "react-icons/fa";
+import xlsx from "json-as-xlsx";
 
-const Navbar = ({setForm,setSelect,select,info,setMultiSelect,handleDelete,multiSelect}) => {
+const Navbar = ({ setSelect, select, info, setMultiSelect, handleDelete, multiSelect }) => {
+  const dropdownStyle={ position: "absolute", top: "50px", right: "10px", background: "#fff", boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)", borderRadius: "8px", zIndex: 1000, padding: "10px"};
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const exportFile = () => {
+    const columns = Object.keys(info[0]).map((key) => ({
+      label: key.charAt(0).toUpperCase() + key.slice(1),
+      value: key,
+    }));
+    const formatedInfo = [{ sheet: "Sheet1", columns, content: info }];
+    const settings = {
+      fileName: "MySpreadsheet",
+      extraLength: 3,
+      writeMode: "writeFile",
+      writeOptions: {},
+      RTL: true,
+    };
+    xlsx(formatedInfo, settings);
+  };
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className='navbar'>
-      <p className='text' style={{fontWeight:"600"}}>Table Devices: {info.length}</p>
-      <div className='operators'>
-        <FaSearch onClick={()=>setSelect(!select)} />
-        <FaTrash onClick={handleDelete}/>
-        <FaRegSquare onClick={()=>setMultiSelect(!multiSelect)}/>
-        <FaSort />
-        <FaList />
-        <button className='btn' style={{color:"cornflowerblue"}}>Scan all</button>
-        <button className='btn' style={{backgroundColor:"cornflowerblue",color:"white"}} onClick={()=>setForm(true)}>Add device</button>
+    <div className="navbar" style={{ position: "relative" }}>
+      <p className="text" style={{ fontWeight: "600" }}>
+        Table Devices: {info.length}
+      </p>
+      <div className="operators">
+        <FaSearch onClick={() => setSelect(!select)} className="nav-buttons" />
+        <FaTrash onClick={handleDelete} className="nav-buttons" />
+        <FaRegSquare onClick={() => setMultiSelect(!multiSelect)} className="nav-buttons" />
+        <FaFileExport onClick={exportFile} className="nav-buttons" />
+        <FaPlus onClick={() => setShowDropdown(!showDropdown)} className="nav-buttons" />
+        {showDropdown && (
+          <div ref={dropdownRef} className="shadow" style={dropdownStyle}>
+            <p>Column Choosing</p>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
